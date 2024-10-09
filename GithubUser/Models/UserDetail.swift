62639d4a0459355e91fbd98296ed08fcc9586c14
LuanDT <thanhluand@gmail.com>
2024-10-09 19:08:50 +0900

    remove unnecessary file

--- GithubUser/Models/UserDetail.swift
+++ GithubUser/Models/UserDetail.swift
@@ -7,40 +7,6 @@
 
 import Foundation
 
-//{
-//    "login": "sbecker",
-//    "id": 1001,
-//    "node_id": "MDQ6VXNlcjEwMDE=",
-//    "avatar_url": "https://avatars.githubusercontent.com/u/1001?v=4",
-//    "gravatar_id": "",
-//    "url": "https://api.github.com/users/sbecker",
-//    "html_url": "https://github.com/sbecker",
-//    "followers_url": "https://api.github.com/users/sbecker/followers",
-//    "following_url": "https://api.github.com/users/sbecker/following{/other_user}",
-//    "gists_url": "https://api.github.com/users/sbecker/gists{/gist_id}",
-//    "starred_url": "https://api.github.com/users/sbecker/starred{/owner}{/repo}",
-//    "subscriptions_url": "https://api.github.com/users/sbecker/subscriptions",
-//    "organizations_url": "https://api.github.com/users/sbecker/orgs",
-//    "repos_url": "https://api.github.com/users/sbecker/repos",
-//    "events_url": "https://api.github.com/users/sbecker/events{/privacy}",
-//    "received_events_url": "https://api.github.com/users/sbecker/received_events",
-//    "type": "User",
-//    "site_admin": false,
-//    "name": "Scott Becker",
-//    "company": "@olioapps",
-//    "blog": "https://www.olioapps.com",
-//    "location": null,
-//    "email": null,
-//    "hireable": null,
-//    "bio": null,
-//    "twitter_username": "sbecker",
-//    "public_repos": 42,
-//    "public_gists": 9,
-//    "followers": 66,
-//    "following": 24,
-//    "created_at": "2008-02-26T23:03:22Z",
-//    "updated_at": "2024-08-06T18:33:53Z"
-//}
 struct UserDetail: Decodable {
     var login: String
     var id: Int
